dc4445a0b66e128cca8bb71d607742f65a2f506a
jackerbell <jackerbell@naver.com>
2023-05-25 14:08:13 +0900

    token expiration and auto logoout, finally complete project

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const token = useLoaderData(); // 이미 루트 라우트에 대해 렌더링 되는 컴포넌트에 있으므로 라우트로더 대신 사용
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(()=>{
+    if(!token) {
+      return;
+    }
+
+    if(token === 'EXPIRED'){
+      submit(null,{action:'/logout',method:'post'});
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null,{
+        action:'/logout',method:'post'})
+    }, tokenDuration); // 1시간 뒤 토큰 만료, 자동 로그아웃!
+  },[token, submit]);
+  
 
   return (
     <>
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,28 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storedExpriationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpriationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime(); // 토큰이 유효하다면 양수, 만료면 음수
+  return duration;
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem('token');
+
+
+  if(!token){
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+  if(tokenDuration < 0){
+    return 'EXPIRED';
+  }
+
+
+
   return token;
 }
 
